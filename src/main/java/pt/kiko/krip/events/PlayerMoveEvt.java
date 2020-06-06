20a85225cda703eb1e420a6152e71a435c9d275a
KikoPT1234 <carricokiko2006@gmail.com>
2020-06-06 11:50:55 +0200

    static block for PlayerMoveEvt.java

--- src/main/java/pt/kiko/krip/events/PlayerMoveEvt.java
+++ src/main/java/pt/kiko/krip/events/PlayerMoveEvt.java
@@ -4,14 +4,18 @@ import org.bukkit.event.Event;
 import org.bukkit.event.player.PlayerMoveEvent;
 import pt.kiko.krip.Krip;
 import pt.kiko.krip.lang.EventInfo;
-import pt.kiko.krip.lang.values.NullValue;
 import pt.kiko.krip.lang.values.ObjectValue;
 import pt.kiko.krip.objects.LocationObj;
 import pt.kiko.krip.objects.PlayerObj;
 
 import java.util.HashMap;
+import java.util.Objects;
 
 public class PlayerMoveEvt extends EventInfo {
+
+    static {
+        Krip.registerEvent(new PlayerMoveEvt());
+    }
 
     public PlayerMoveEvt() {
         super("PlayerMoveEvent", PlayerMoveEvent.class);
@@ -20,10 +24,10 @@ public class PlayerMoveEvt extends EventInfo {
     @Override
     protected ObjectValue getEvent(Event event) {
         assert event instanceof PlayerMoveEvent;
-        PlayerMoveEvent evt = (PlayerMoveEvent)event;
+        PlayerMoveEvent evt = (PlayerMoveEvent) event;
 
         ObjectValue eventObj = new ObjectValue(new HashMap<>(), Krip.context);
-        eventObj.set("to", new LocationObj(evt.getTo(), Krip.context));
+        eventObj.set("to", new LocationObj(Objects.requireNonNull(evt.getTo()), Krip.context));
         eventObj.set("from", new LocationObj(evt.getFrom(), Krip.context));
         eventObj.set("player", new PlayerObj(evt.getPlayer(), Krip.context));
         return eventObj;
